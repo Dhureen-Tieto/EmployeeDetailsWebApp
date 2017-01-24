e83ef4ed3aaec17e01bf100be48d1f5799ffc783
Dhureen <dhureen.m@gmail.com>
2017-01-24 14:38:28 +0530

    changes to MainWindow.xaml and ViewModel.cs

--- EmployeeApiClient/EmployeeClientViewModel.cs
+++ EmployeeApiClient/EmployeeClientViewModel.cs
@@ -14,33 +14,7 @@ namespace EmployeeApiClient
 {
     class EmployeeClientViewModel
     {
-        private void BindEmployeeList()
-        {
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("http://localhost:3339/");
-            //client.DefaultRequestHeeaders.Add("appkey", "myapp_key");
-            client.DefaultRequestHeaders.Accept.Add(
-               new MediaTypeWithQualityHeaderValue("application/json"));
-
-            HttpResponseMessage response = client.GetAsync("api/employee").Result;
-            if (response.IsSuccessStatusCode)
-            {
-                var employees = response.Content.ReadAsAsync<IEnumerable<Employee>>().Result;
-                employeeGrid.ItemsSource = employees;
-
-            }
-            else
-            {
-                MessageBox.Show("Error Code" + response.StatusCode + " : Message - " + response.ReasonPhrase);
-            }
-        }
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            BindEmployeeList();
-        }
-        private void btnAdd_Click(object sender, RoutedEventArgs e)
-        {
-        }
+        
     }
  }
 
